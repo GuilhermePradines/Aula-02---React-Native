adbc98bd29db3e0eacab92df8d41a36599711ae1
GuilhermePradines <guilhermepradines05@gmail.com>
2025-04-02 22:28:23 -0300

    Telas

--- App.js
+++ App.js
@@ -1,22 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { Input, Button, Avatar } from "react-native-elements";
 
-export default function App() {
+const Login = () => {
   return (
-    <View style={[styles.container, {
-      flexDirection: "column"
-      }]}>
-      <View style={{ flex: 1, backgroundColor: "red" }} />
-      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-      <View style={{ flex: 3, backgroundColor: "green" }} />
-      </View>
-      );
-      };
+    <View style={styles.container}>
+      <Avatar
+        rounded
+        size="large"
+        icon={{ name: "user", type: "font-awesome" }}
+        containerStyle={styles.avatar}/>
+      <Input label="Email" placeholder="Digite seu email" />
+      <Input label="Senha" placeholder="Digite sua senha" secureTextEntry />
+      <Button title="Logar" buttonStyle={styles.button} />
+      <Button title="Cadastre-se" buttonStyle={styles.button} />
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>esqueceu a senha</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Cadastro = () => {
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.title}>
+        Cadastro
+      </Text>
+
+      <Input label="Nome" placeholder="Digite seu nome" />
+
+      <Input label="Email"placeholder="Digite seu email"keyboardType="email-address"/>
+
+      <Input label="Senha" placeholder="Digite sua senha" secureTextEntry />
+
+      <Button title="Cadastrar" buttonStyle={styles.button} />
+    </View>
+  );
+};
+
+const ForgotPassword = () => {
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.title}>
+        Esqueceu a senha
+      </Text>
+      <Input label="Email" placeholder="Digite seu email" keyboardType="email-address"/>
+      <Button title="Enviar" buttonStyle={styles.button} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  padding: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
-  });
+  avatar: {
+    marginBottom: 20,
+  },
+  button: {
+    width: 200,
+    marginVertical: 10,
+    backgroundColor: "#2ECC71",
+  },
+  forgotPassword: {
+    marginTop: 10,
+    color: "black",
+    fontWeight: "bold",
+  },
+});
+
+export default ForgotPasswordScreen;
